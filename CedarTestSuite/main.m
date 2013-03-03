32c75ff85d220efd96ba8f3b61688aba29d3d06a
Tonny Xu <tonny.xu@gmail.com>
2013-03-03 18:04:50 +0900

    Added Cedar TestSuite, which is an iOS app can run on device.

---
+++ CedarTestSuite/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CedarTestSuite
+//
+//  Created by Tonny Xu on 3/3/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+  }
+}
